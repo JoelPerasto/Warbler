28abfbb7210d7e00de77a813fac2bdef1bbc1151
Joel <joel.perasto2@gmail.com>
2019-04-28 19:53:44 +0300

    initial

---
+++ warbler-server/routes/messages.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const router = express.Router({mergeParams: true});
+
+const {
+    createMessage,
+    getMessage,
+    deleteMessage
+} = require("../handlers/messages");
+
+//prefix - /api/user/:id/messages
+router.route("/").post(createMessage);
+
+//prefix - /api/user/:id/messages/:messages_id
+router
+    .route("/:message_id")
+    .get(getMessage)
+    .delete(deleteMessage);
+
+    
+module.exports = router;
